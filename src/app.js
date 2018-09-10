d1f7df697fe05d267a3ab4a2b48d4bfa4e41b5b8
Monisha Shah <monishashah06@gmail.com>
2018-09-10 14:07:06 -0700

    Adding new test cases developed

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 store.dispatch(addExpense({description : 'Water bill', amount: 1000, createdAt: -121000}));
